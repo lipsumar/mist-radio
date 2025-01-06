53e8af00475aab59c8638149740acd6c180266b8
Emmanuel Pire <piremmanuel@gmail.com>
2025-01-06 09:31:02 +0100

    remove debug

--- src/lib/play.ts
+++ src/lib/play.ts
@@ -7,28 +7,23 @@ export async function playSample(
   const AudioContext =
     window.AudioContext || (window as any).webkitAudioContext;
   const audioContext = new AudioContext();
-  console.log("audioContext", audioContext);
+
   try {
-    console.log("audiocontext state", audioContext.state);
     // Resume audio context - needed for iOS
     if (audioContext.state === "suspended") {
-      console.log("resuming audio context");
       await audioContext.resume();
     }
 
     // Convert blob to array buffer
     const arrayBuffer = await blob.arrayBuffer();
-    console.log("arrayBuffer", arrayBuffer);
 
     // Decode the audio data
     const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
-    console.log("audioBuffer", audioBuffer);
 
     // Create buffer source
     const source = audioContext.createBufferSource();
     source.buffer = audioBuffer;
     source.connect(audioContext.destination);
-    console.log("source", source);
 
     // Start playback
     if (end !== null) {
@@ -37,11 +32,8 @@ export async function playSample(
       source.start(0);
     }
 
-    console.log("source started");
-
     // Clean up when done
     source.onended = () => {
-      console.log("source ended");
       source.disconnect();
       audioContext.close();
     };
